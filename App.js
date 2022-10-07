ddc086f893088715b069855ac87244d4d980f8a8
Williams Amaya <whistemalo@gmail.com>
2022-10-07 11:22:15 -0600

    instalcion de nativewind & botton navigation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './Components/HomeScreen';
+import SettingsScreen from './Components/SettingsScreen';
+
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Components/SettingsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SettingsScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>SettingsScreen</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
