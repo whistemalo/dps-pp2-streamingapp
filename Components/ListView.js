56da2a90990cc109a963b90b259bb6dd9cb5a7a7
Williams Amaya <whistemalo@gmail.com>
2022-10-08 18:12:29 -0600

    flat list de las peliculas maquetado

--- Components/ListView.js
+++ Components/ListView.js
@@ -1,31 +1,31 @@
 import { View, Text,FlatList,Image,TouchableOpacity } from 'react-native'
-import Images from '../assets/Images';
 
-const ListView = ({data,setReference,type}) => {
+
+const ListView = ({data,setReference}) => {
   
  console.log(data)
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity className="border items-center justify-center w-[100px] h-24 bg-slate-800 rounded-lg"
+    <TouchableOpacity className="border items-center justify-center w-[100px] h-32 bg-slate-800 rounded-lg "
     onPress={()=> setReference(item.rel)}>
-      <Text className="text-white">{item.englishName}</Text>
-      <Image className="mt-2 w-10 h-10"
-        source={Images[item.img]}
-      />
+      <View className="relative flex w-[100px] items-center py-4">
+        <Image  className="w-full h-16 mb-2"  source={{
+          uri: item.i.imageUrl}}/>
+        <Text className="text-white  text-center mx-2">{item.l}</Text>
+      </View>
     </TouchableOpacity>
   );
 
 
   return (
-    <View className="mt-10 mr-3 ml-3 ">
+    <View className="mt-2 mr-3 ml-3 ">
        
         <FlatList
           ItemSeparatorComponent={() => (
             <View className="w-2" />
           )}
-          className=""
           horizontal={true}
-          data={data}
+          data={data.d}
           renderItem={renderItem}
           keyExtractor={data => data.id}
         />
--- screens/PeliculaScreen.js
+++ screens/PeliculaScreen.js
@@ -1,36 +1,38 @@
-import { Text, View, Image, ScrollView} from 'react-native'
+import { Text, View, Image, ScrollView,SafeAreaView} from 'react-native'
 import React, { Component } from 'react'
+import ListView from '../Components/ListView'
+import * as comedia from '../movies/comedia.json';
+import * as accion from '../movies/accion.json';
+import * as romance from '../movies/romance.json';
 
-export class PeliculaScreen extends Component {
-  render() {
-    return (
-        <View className="flex-1 bg-white">
-          <View className=" items-center">
-            <Image className="w-96 h-52" source={require('../assets/streaming.jpg')}/>
+
+
+
+const PeliculaScreen = () => {
+
+  return (
+    <SafeAreaView className="flex  bg-white">
+      <ScrollView>
+      <View className=" items-center w-96 h-52">
+            {/* <Text className="text-red-500">tesadsada</Text> */}
+            <Image  className="w-96 h-52"  source={require('../assets/streaming.jpg')}/>
           </View>
           <View>
-            <Text className="text-lg font-semibold mt-6 mx-4">Comedia</Text>
-            <ScrollView horizontal>
-              <View>
-                <Image source={require('../assets/comedia/pelicula1.jpg')}/>
-              </View>
-              <View>
-                <Image source={require('../assets/comedia/pelicula2.jpg')}/>
-              </View>
-              <View>
-                <Image source={require('../assets/comedia/pelicula3.jpg')}/>
-              </View>
-              <View>
-                <Image source={require('../assets/comedia/pelicula4.jpg')}/>
-              </View>
-              <View>
-                <Image source={require('../assets/comedia/pelicula5.jpg')}/>
-              </View>
-            </ScrollView>
+            <Text className="text-lg mt-4 font-semibold  mx-4">Comedia</Text>
+            <ListView data={comedia}/>
           </View>
-        </View>
-    )
-  }
+          <View>
+            <Text className="text-lg mt-4 font-semibold  mx-4">Acción</Text>
+            <ListView data={accion}/>
+          </View>
+          <View>
+            <Text className="text-lg mt-4 font-semibold  mx-4">Romance</Text>
+            <ListView data={romance}/>
+          </View>
+      </ScrollView>
+          
+        </SafeAreaView>
+  )
 }
 
 export default PeliculaScreen
