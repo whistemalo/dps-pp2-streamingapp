c0b11e82df719fef541fa003200b8c1c0a4a22cf
RubidiaPaz <rubigarcia190@gmail.com>
2022-10-08 23:43:44 -0600

    Iconos del menu

--- App.js
+++ App.js
@@ -5,15 +5,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PeliculaScreen from './screens/PeliculaScreen';
 import SerieScreen from './screens/SerieScreen';
 import ProximamenteScreen from './screens/ProximamenteScreen'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+  
 
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Peliculas" component={PeliculaScreen} />
-        <Tab.Screen name="Series" component={SerieScreen} />
-        <Tab.Screen name="Proximamente" component={ProximamenteScreen} />
+        <Tab.Screen name="Peliculas" component={PeliculaScreen} 
+        options={{ tabBarLabel: 'Peliculas', tabBarIcon: ({color, size}) => (
+          <Icon name="play-circle" color={color} size={25}/>
+        ),}}/>
+        <Tab.Screen name="Series" component={SerieScreen}
+        options={{ tabBarLabel: 'Series', tabBarIcon: ({color, size}) => (
+          <Icon name="film" color={color} size={25}/>
+        ),}} />
+        <Tab.Screen name="Proximamente" component={ProximamenteScreen} 
+        options={{ tabBarLabel: 'Proximamente', tabBarIcon: ({color, size}) => (
+          <Icon name="videocam" color={color} size={25}/>
+        ),}}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
